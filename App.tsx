ad8ab8b8a176b8ad863aad1e051d17c3944ccc6a
davi-i <josedavifrancelino@gmail.com>
2022-09-23 17:25:43 -0300

    feat: added navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Espiaum</Text>
+            <Button title="Iniciar jogo" onPress={() => navigation.navigate('Game')}/>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+function GameScreen() {
+    return (
+        <View>
+            <Text>It's the game!</Text>
+        </View>
+    )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Game" component={GameScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
